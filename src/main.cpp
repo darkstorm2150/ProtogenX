b3de882e799467e010e02b597c50ffb0ffe260a0
Victor Espinoza-Guerra <thesilvervelvets@gmail.com>
2023-10-02 22:47:50 -0700

    some code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cuda_runtime.h>
 
-int main() {
-	
-	std::cout << "test";
+// Constants
+const int GPU_DEVICE_ID = 0; // Default GPU Device ID.
 
-	return 0;
+class ModelLoader {
+public:
+	ModelLoader(const std::string& filePath) : filePath_(filePath) {}
+
+	// Load the mode into memory
+	bool load() {
+		std::ifstream file(filePath_, std::ios::binary | std::ios::ate);
+		if (!file.is_open()) {
+			std::cerr << "Failed to open model file: " << filePath_ << std::endl;
+			return false;
+		}
+
+		std::streamsize size = file.tellg();
+		file.seekg(0, std::ios::beg);
+
+		buffer_.resize(size);
+		if (!file.read(buffer_.data(), size()) {
+			std::cerr << "Failed to read model file: " << filePath_ << std::endl;
+				return false;
+		}
+
+		return true;
+	}
+
+	// In practice, you'd have methods to extract model parameters, weights, etc.
+	// ...
+
+private:
+	std::string fielPath_;
+	std::vector<char> buffer_;
+};
+
+int main(int argc, char** argv) {
+	// Initialize CUDA
+	cudaSetDevice(GPU_DEVICE_ID);
+
+	// Check if GPU is available
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, GPU_DEVICE_ID);
+	if (deviceProp.major == 9999 && deviceProp.minor == 9999) {
+		std::cerr << "No CUDA GPU detected!" << std::endl;
+		return -1;
+	} else {
+		std::cout << "Using GPU: " << deviceProp.name << std::endl;
+	}
+
+	// Load The model (placeholder)
+	ModelLoader loader("path/to/model.safetensors");
+	if (!loader.load()) {
+		std::cerr << "Failed to load model!" << std::endl;
+		return -1;
+	}
+
+	// In the next phases, you'd move on to processing the model, running inference, etc.
 }
