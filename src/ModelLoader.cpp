eea51e8c874cd225c33ab51d63bd22557b63b1b5
Victor Espinoza-Guerra <thesilvervelvets@gmail.com>
2023-10-03 00:04:58 -0700

    Basic Model Loading

--- src/ModelLoader.cpp
+++ src/ModelLoader.cpp
@@ -3,20 +3,21 @@
 ModelLoader::ModelLoader(const std::string& filePath) : filePath_(filePath) {}
 
 bool ModelLoader::load() {
-	std::ifstream file(filePath_, std::ios::binary | std::ios::ate);
-	if (!file.is_open()) {
-		std::cerr << "Failed to open model file: " << filePath_ << std::endl;
-		return false;
-	}
+    std::ifstream file;
+    file.open(filePath_, std::ios::binary | std::ios::ate);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open model file: " << filePath_ << std::endl;
+        return false;
+    }
 
-	std::streamsize size = file.tellg();
-	file.seekg(0, std::ios::beg);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
 
-	buffer_.resize(size);
-	if (!file.read(buffer_.data(), size)) {
-		std::cerr << "Failed to read model file: " << filePath_ << std::endl;
-		return false;
-	}
+    buffer_.resize(size);
+    if (!file.read(buffer_.data(), size)) {
+        std::cerr << "Failed to read model file: " << filePath_ << std::endl;
+        return false;
+    }
 
-	return true;
+    return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,54 @@
 #include <iostream>
 #include <cuda_runtime.h>
+#include <filesystem>  // for file existence check
 #include "ModelLoader.h"
 
 // Constants
 const int GPU_DEVICE_ID = 0; // Default Device ID
 
 int main(int argc, char** argv) {
-	// Initialize CUDA
-	cudaSetDevice(GPU_DEVICE_ID);
+    // Initialize CUDA
+    cudaError_t err = cudaSetDevice(GPU_DEVICE_ID);
+    if (err != cudaSuccess) {
+        std::cerr << "Failed to set CUDA device: " << cudaGetErrorString(err) << std::endl;
+        return -1;
+    }
 
-	// Check if GPU is avaiable
-	cudaDeviceProp deviceProp;
-	cudaGetDeviceProperties(&deviceProp, GPU_DEVICE_ID);
-	if (deviceProp.major == 9999 && deviceProp.minor == 9999) {
-		std::cerr << "No CUDA GPU detected!" << std::endl;
-		return -1;
-	} else {
-		std::cout << "Using GPU: " << deviceProp.name << std::endl;
-	}
+    // Check if GPU is available
+    cudaDeviceProp deviceProp;
+    err = cudaGetDeviceProperties(&deviceProp, GPU_DEVICE_ID);
+    if (err != cudaSuccess) {
+        std::cerr << "Failed to get device properties: " << cudaGetErrorString(err) << std::endl;
+        return -1;
+    }
 
-	// Load the model (placeholder)
-	ModelLoader loader("path/to/model.safetensor");
-	if (!loader.load()) {
-		std::cerr << "Failed to load model!" << std::endl;
-		return -1;
-	}
+    if (deviceProp.major == 9999 && deviceProp.minor == 9999) {
+        std::cerr << "No CUDA GPU detected!" << std::endl;
+        return -1;
+    }
+    else {
+        if (deviceProp.major < 3) {
+            std::cerr << "CUDA capability major version is less than 3, which might not support all CUDA features used in this program." << std::endl;
+            return -1;
+        }
+        std::cout << "Using GPU: " << deviceProp.name << std::endl;
+    }
 
-	// Future phases: processing the model, running inference, etc.
+    // Verify File Existence
+    std::string modelPath = "C:\\Users\\thesi\\source\\repos\\Protogen X\\x64\\Debug\\Model.safetensors";
+    if (!std::filesystem::exists(modelPath)) {
+        std::cerr << "Model file does not exist at the specified path!" << std::endl;
+        return -1;
+    }
+
+    // Load the model
+    ModelLoader loader(modelPath);
+    if (!loader.load()) {
+        std::cerr << "Failed to load model!" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Model loaded successfully!" << std::endl;
+
+    // Future phases: processing the model, running inference, etc.
 }
